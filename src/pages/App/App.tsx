368f0526b885cbb88e85b905a425e0e11a8b327d
Dyv1k <duvanov.ilja@gmail.com>
2023-09-02 05:48:37 +0300

    fix

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -2,13 +2,15 @@ import { FC } from 'react'
 
 import TodoList from '../../widgets/TodoList/TodoList';
 
+import logo_img from '../../images/logo.svg'
+
 import './App.scss';
 
 const App: FC = () => {
 	return (
 		<div className="app">
 			<header className="app-header">
-				<img src={"/logo.svg"} className="app-logo" alt="logo" />
+				<img src={logo_img} className="app-logo" alt="logo" />
 			</header>
 			<main className="app-content">
 				<section className="app-todo">
